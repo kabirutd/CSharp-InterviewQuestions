485dce565ad0204586d7981af4b0e42cdd5d50db
Mohammad Kabir <mohammadkabir@gmail.com>
2019-08-18 13:08:21 -0500

    08172019

--- SwapNumber/Program.cs
+++ SwapNumber/Program.cs
@@ -6,9 +6,20 @@ namespace SwapNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = 2;
+            int num2 = 3;
+
+            Console.WriteLine($"First Number before swapping: {num1}");
+            Console.WriteLine($"Second Number before swapping: {num2}");
+
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            Console.WriteLine($"First Number after swapping: {num1}");
+            Console.WriteLine($"Second Number after swapping: {num2}");
         }
 
-
+    
     }
 }
