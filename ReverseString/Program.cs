187996e2b32738946416ea21ba06f347b15b1d71
Mohammad Kabir <mohammadkabir@gmail.com>
2019-08-18 23:23:13 -0500

    08182019

---
+++ ReverseString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Reverse This!";
+            Console.WriteLine(DoReverseString(str));
+        }
+
+        static string DoReverseString(string str)
+        {
+
+            return new string(str.ToCharArray().Reverse().ToArray());
+            
+        }
+    }
+}
