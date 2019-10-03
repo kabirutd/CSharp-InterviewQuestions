82b5d8b3ae17614bae7ce255d8ef981b663b7c1d
kabirutd <mohammadkabir@gmail.com>
2019-10-02 20:11:37 -0500

    10022019

---
+++ Generics/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instance of string type 
+            GFG<string> name = new GFG<string>();
+            name.value = "Mohammad Kabir";
+
+            // instance of float type 
+            GFG<float> version = new GFG<float>();
+            version.value = 5.0F;
+
+            // display Mohammad Kabir 
+            Console.WriteLine(name.value);
+
+            // display 5 
+            Console.WriteLine(version.value);
+        }
+    }
+
+    // We use < > to specify Parameter type 
+    public class GFG<T>
+    {
+
+        // private data members 
+        private T data;
+
+        // using properties 
+        public T value
+        {
+
+            // using accessors 
+            get
+            {
+                return this.data;
+            }
+            set
+            {
+                this.data = value;
+            }
+        }
+    }
+}
