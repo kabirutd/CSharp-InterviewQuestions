381cb0628a8f3b5f614c0454c542baa51823f9ae
kabirutd <mohammadkabir@gmail.com>
2019-10-02 20:24:55 -0500

    10022019

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -19,8 +19,29 @@ namespace Generics
 
             // display 5 
             Console.WriteLine(version.value);
+
+            // creating object of class GFGMethod 
+            //=============================================================================
+            GFGMethod p = new GFGMethod();
+
+            // calling Generics method 
+            p.Display<int>("Integer", 122);
+            p.Display<char>("Character", 'H');
+            p.Display<double>("Decimal", 255.67);
+            //return 0;
         }
     }
+
+    public class GFGMethod
+    {
+        // Generics method 
+        public void Display<GenericType>(string msg, GenericType value)
+        {
+            Console.WriteLine("{0}:{1}", msg, value);
+        }
+    }
+
+
 
     // We use < > to specify Parameter type 
     public class GFG<T>
