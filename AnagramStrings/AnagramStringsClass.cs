a30de03475e9244983febc30f9a82659350b052f
kabirutd <mohammadkabir@gmail.com>
2019-10-10 08:21:20 -0500

    10102019

--- AnagramStrings/AnagramStringsClass.cs
+++ AnagramStrings/AnagramStringsClass.cs
@@ -32,5 +32,16 @@ namespace AnagramStrings
 
         }
 
+        /*
+         
+        //Javascript:
+        function bool isAnagram(str1, str2)
+        {
+
+        return (str1.split('').sort().join('') === str2.split('').sort().join('') ? true: false );
+
+        }
+         * */
+
     }
 }
